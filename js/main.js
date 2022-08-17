3d3e14970582fa064b49f828fa32cbf431632a4e
Joa <86939873+joa-jo@users.noreply.github.com>
2022-08-17 13:40:20 +0900

    feat: 특정 타이밍 스크롤 애니메이션 기능 추가

--- js/main.js
+++ js/main.js
@@ -20,7 +20,8 @@
 				messageD: document.querySelector('#scroll-section-0 .main-message.d'),
 			},
 			values: {
-				messageA_opacity: [0, 1]
+				messageA_opacity: [0, 1, { start: 0.1, end: 0.2 }],
+				messageB_opacity: [0, 1, { start: 0.3, end: 0.4 }],
 			}
 		},
 		{
@@ -75,8 +76,25 @@
 	function calcValues(values, currentYOffset) {
 		let rv;
 		// 현재 씬(스크롤 섹션)에서 스크롤된 범위를 비율로 계산한 값
-		let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight
-		rv = scrollRatio * (values[1] - values[0]) + values[0]
+		const scrollHeight = sceneInfo[currentScene].scrollHeight
+		const scrollRatio = currentYOffset / scrollHeight
+
+		if (values.length === 3) {
+			// start와 end 사이에 애니메이션 실행
+			const partScrollStart = values[2].start * scrollHeight
+			const partScrollEnd = values[2].end * scrollHeight
+			const partScrollHeight = partScrollEnd * partScrollStart
+
+			if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+				rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0]
+			} else if (currentYOffset < partScrollStart) {
+				rv = values[0]
+			} else if (currentYOffset > partScrollEnd) {
+				rv = values[1]
+			}
+		} else {
+			rv = scrollRatio * (values[1] - values[0]) + values[0]
+		}
 		return rv;
 	}
 
