f13e7f7d4afe3ac9c5056a73ebafbe65c1542aa1
Joa <86939873+joa-jo@users.noreply.github.com>
2022-09-26 14:08:02 +0900

    feat: 흰색 여백 계산

--- js/main.js
+++ js/main.js
@@ -112,7 +112,9 @@
 				images: []
 			},
 			values: {
-	
+				// 양 옆 흰색 여백
+				rect1X: [0, 0, {start: 0, end: 0}],
+				rect2X: [0, 0, {start: 0, end: 0}],
 			}
 		}
 	];
@@ -316,10 +318,23 @@
 				}
 				objs.canvas.style.transform = `scale(${canvasScaleRatio})`
 				objs.context.drawImage(objs.images[0], 0, 0)
+
+				const recalculatedInnerWidth = window.innerWidth / canvasScaleRatio
+				const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio
+				
+				const whiteRectWidth = recalculatedInnerWidth * 0.15
+				values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2
+				values.rect1X[1] = values.rect1X[0] - whiteRectWidth
+				values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth
+				values.rect2X[1] = values.rect2X[0] + whiteRectWidth
+
+				// 좌우 흰색 박스 그리기 (x, y, width(정수 처리), height)
+				objs.context.fillRect(values.rect1X[0], 0, parseInt(whiteRectWidth), objs.canvas.height)
+				objs.context.fillRect(values.rect2X[0], 0, parseInt(whiteRectWidth), objs.canvas.height)
+
 				break;
 		}
 	}
-	
 
 	function scrollLoop() {
 		enterNewScene = false;
